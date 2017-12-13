7f60f812fa414613b833e1f45eb9f849b4877f3e
Daniel-Cholewa <34522186+Daniel-Cholewa@users.noreply.github.com>
2017-12-13 16:15:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+//Created by:  Daniel Cholewa
+//Dec 10th 2017
+/* a simple dice game where you roll and hold your score the first player to 50 wins 
+rolling a 1 will give the turn to the next player. 2 6s in a row you lose all ponits
+*/
+
+//all vars used in the code
 var scores, roundScore, activePlayer, maxScore, playerNames, gameOver, prevDiceRoll;
 var diceDom = document.querySelector('.dice');
 
-
+//Resets the game and starts a new game
 function newGame() {
     playerNames = ['',''];
-    playerNames[0] = prompt('What is the name of player 1?');
-    playerNames[1] = prompt('What is the name of player 2?');
+    playerNames[0] = prompt('What is the name of player 1?','Player 1');
+    playerNames[1] = prompt('What is the name of player 2?','Player 2');
     //maxScore = prompt('How many ponits to you want to play for?');
     maxScore = 50;
     document.getElementById('name-0').textContent = playerNames[0];
@@ -22,20 +29,25 @@ function newGame() {
     document.getElementById('message-0').textContent = 'First to ' + maxScore + ' wins';
     document.getElementById('message-1').textContent = 'First to ' + maxScore + ' wins';
     document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.btn-roll').style.display = 'block';
-    document.querySelector('.btn-hold').style.display = 'block';
+    
+    displayHoldRoll('block');
     setCurrent();
 }
 
+//Hides or displayes hide / roll button
+function displayHoldRoll (el){
+    document.querySelector('.btn-roll').style.display = el;
+    document.querySelector('.btn-hold').style.display = el;
+}
 
+//Sets currents scores to 0
 function setCurrent (){
     document.getElementById('current-0').textContent = 0;
     document.getElementById('current-1').textContent = 0;
 }
 
+//Changes the player
 function changePlayer(){
-    
-
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
     prevDiceRoll = 0;
@@ -48,9 +60,11 @@ function changePlayer(){
     
 }
 
+
+//Start of Code
 newGame();
-setCurrent();
-    
+ 
+//the click event for button roll
 document.querySelector('.btn-roll').addEventListener('click', function(){
     
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -81,23 +95,24 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 });
 
 
-
+//The hold button
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    
-    //Add Scores to active player
     
     scores[activePlayer] += roundScore;
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
     
     if (scores[activePlayer] >= maxScore){
+        
         var newG = confirm('Congrats ' + playerNames[activePlayer] + ' You Won the game! with a score of ' + scores[activePlayer] + ' Do you want to play again?');
-         document.getElementById('message-' + activePlayer).textContent = 'Winner!';
+        
+        document.getElementById('message-' + activePlayer).textContent = 'Winner!';
+        
         gameOver = false;
+        
          if (newG === true){
             newGame();
          } else {
-            document.querySelector('.btn-roll').style.display = 'none';
-            document.querySelector('.btn-hold').style.display = 'none';
+            displayHoldRoll('none');
         }
         
     } else {
@@ -108,6 +123,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     
 });
 
+//New Game button
 document.querySelector('.btn-new').addEventListener('click', function(){
     
 if (gameOver){
